e599b4ba54e8bb178e5340477bb88fad980a4c62
FirstPlace745 <159957473+FirstPlace745@users.noreply.github.com>
2024-04-28 15:12:52 +0300

    Add files via upload

---
+++ homework10.py
@@ -0,0 +1,8 @@
+def print_params(name):
+    print('Ваше Имя:', name)
+    print('Ваше Имя:', name)
+
+
+print_params('Andrew')
+print_params('Max')
+print_params('John')
---
+++ homework9.py
@@ -0,0 +1,8 @@
+cars_list = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
+
+for i in cars_list:
+    print('Я езжу на автомобиле марки', i)
+    cars_count += 10
+    print(cars_count)
+
