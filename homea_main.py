a12f4d2070f12c312df4df3ec853bf446bb27cc9
dizzzha <159957473+dizzzha@users.noreply.github.com>
2024-05-20 15:15:37 +0300

    Add files via upload

---
+++ homea_main.py
@@ -0,0 +1,24 @@
+#  1. Длина Слова
+a = 'Hello, world!'
+print(len(a))
+
+#  2. Суммы и разности
+first, second = 5, 3
+summa = first + second
+diff = first - second
+print(summa, diff)
+
+#  3. Среднее арифметическогое
+first, second, third = 4, 3, 5
+mean = (first + second + third) // 3
+print(mean)
+
+#  4. Простые строчки
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(f'{second_string}, {first_string}')
+
+#  5. Сложная формула
+a, b, c = 1, 2, 3
+f = (a * b) + (a * c)
+print(f**3/2)
