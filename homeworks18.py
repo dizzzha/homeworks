e1664142ac6631e5c461cf6537890f2892327945
dizzzha <159957473+dizzzha@users.noreply.github.com>
2024-05-25 09:30:31 +0300

    Update homeworks18.py

--- homeworks18.py
+++ homeworks18.py
@@ -1,4 +1,4 @@
-class Building:
+class Buiding:
 
     def __init__(self, numberOfFloors, buildingType):
         self.numberOfFloors = numberOfFloors
@@ -8,6 +8,6 @@ class Building:
         return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
 
-first_building = Building(1, 'hello')
-second_building = Building(1, 'hello')
-print(first_building.__eq__(second_building))
+first_buiding = Buiding(1, 'hello')
+second_buiding = Buiding(1, 'hello')
+print(first_buiding.__eq__(second_buiding))
