b2e89ce2a68aa43b164a7bb3dabbf0e6bf0fa319
dizzzha <159957473+dizzzha@users.noreply.github.com>
2024-07-20 10:30:56 +0300

    Update homeworks31_9_3.py

--- homeworks31_9_3.py
+++ homeworks31_9_3.py
@@ -2,7 +2,7 @@ first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
 first_result = (len(frst) - len(scnd) for frst, scnd in zip(first, second) if len(frst) != len(scnd))
-second_result = (len(first[i]) == len(second[i]) for i in range(min(len(first), len(second))))
+second_result = (len(first[i]) == len(second[i]) for i in range(len(second)))
 
 print(list(first_result))
 print(list(second_result))
